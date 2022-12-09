d3a75ab7bab3151ea7f8ae1cdb88ae5041cf604c
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-09 10:51:02 -0600

    221209_1050 begin

--- solutions.js
+++ solutions.js
@@ -8,3 +8,75 @@
  * here. If you do, the automated tests will not be able to see your functions.
  */
 
+// p01
+//Define a function named codingIs that takes in one string value and returns that value concatenated to the phrase "Coding is ". If the input value is not a string, the function should return the string "That is not a string"
+
+function codingIs(a){
+    if (typeof a === `string`){
+        return `Coding is ${a}`
+    }
+    return `That is not a string`
+}
+
+//p02
+// Define a function named isGreaterThan that takes in two values and returns a true if the first argument is greater than the second argument otherwise it should return false. If either argument is not a number, return false.
+
+function (){
+
+}
+
+//p03
+// Define a function named isLessThan that takes in two values and returns a true if the first argument is less than the second argument otherwise it should return false. If either argument is not a number, return false.
+
+function (){
+
+}
+
+//p04
+// Define a function named isEqualNumber that takes in two values and returns a true if the first argument is equal to the second argument, otherwise it should return false. If either argument is not a number return false.
+
+function (){
+
+}
+
+//p05
+// Define a function named isEqual that takes in two values and returns a true if the first argument has the same value and type as the second argument, otherwise it should return false.
+
+function (){
+
+}
+
+//p06
+// Define a function named isNot that accepts a boolean argument and returns the opposite value of the argument. It should return false if either argument is null or not a boolean.
+
+function (){
+
+}
+
+//p07
+// Define a function named calcCube that accepts a number argument and returns the cubed value of the argument. It should return O (zero) if either argument is null or not a number.
+
+function (){
+
+}
+
+//p08
+// Define a function named isTypeMatch that accepts two arguments and returns true if the types of the arguments match.
+
+function (){
+
+}
+
+//p09
+// Define a function named findLongestString that accepts two string arguments and returns the string with the greatest length. If only one argument is a string, return that argument. If neither arguments are strings, return and empty string.
+
+function (){
+
+}
+
+//p10
+// Define a function named isFactor that accepts two number arguments and returns true if the first argument is a factor of the second argument. It should return false if either argument is null or not a number. Also return null if the first argument is 0 (zero).
+
+function (){
+
+}
