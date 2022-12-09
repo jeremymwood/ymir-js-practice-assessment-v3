603f3cb0f42374806df05931947a1ab491410fd0
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-09 10:53:35 -0600

    221209_1050 stop work, redundant

--- solutions.js
+++ solutions.js
@@ -21,62 +21,62 @@ function codingIs(a){
 //p02
 // Define a function named isGreaterThan that takes in two values and returns a true if the first argument is greater than the second argument otherwise it should return false. If either argument is not a number, return false.
 
-function (){
-
+function isGreaterThan(a,b){
+    if ()
 }
 
 //p03
 // Define a function named isLessThan that takes in two values and returns a true if the first argument is less than the second argument otherwise it should return false. If either argument is not a number, return false.
 
-function (){
+function isLessThan(){
 
 }
 
 //p04
 // Define a function named isEqualNumber that takes in two values and returns a true if the first argument is equal to the second argument, otherwise it should return false. If either argument is not a number return false.
 
-function (){
+function isEqualNumber(){
 
 }
 
 //p05
 // Define a function named isEqual that takes in two values and returns a true if the first argument has the same value and type as the second argument, otherwise it should return false.
 
-function (){
+function isEqual(){
 
 }
 
 //p06
 // Define a function named isNot that accepts a boolean argument and returns the opposite value of the argument. It should return false if either argument is null or not a boolean.
 
-function (){
+function isNot(){
 
 }
 
 //p07
 // Define a function named calcCube that accepts a number argument and returns the cubed value of the argument. It should return O (zero) if either argument is null or not a number.
 
-function (){
+function calcCube(){
 
 }
 
 //p08
 // Define a function named isTypeMatch that accepts two arguments and returns true if the types of the arguments match.
 
-function (){
+function isTypeMatch(){
 
 }
 
 //p09
 // Define a function named findLongestString that accepts two string arguments and returns the string with the greatest length. If only one argument is a string, return that argument. If neither arguments are strings, return and empty string.
 
-function (){
+function findLongestString(){
 
 }
 
 //p10
 // Define a function named isFactor that accepts two number arguments and returns true if the first argument is a factor of the second argument. It should return false if either argument is null or not a number. Also return null if the first argument is 0 (zero).
 
-function (){
+function isFactor(){
 
 }
